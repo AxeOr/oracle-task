bfa5054de59906b699dc9f3e5e949d3b5a96aa35
AxeOr <axeorst@gmail.com>
2020-09-23 20:50:57 +0300

    Finding first step by Ordinal, looping through followers array; Problems with CSS positioning

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const URL =
   "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
 const requestType = "GET";
 const dataTypeExpected = "jsonp";
+let globalStepsArray = [];
 
 const getJsonData = (url) => {
   return $.ajax({
@@ -17,11 +18,34 @@ const getJsonData = (url) => {
   });
 };
 
-const createTipDiv = (stepsData, stepIndex, stepsCount, tiplates) => {
-  // need to check index here, eventhough there shouldnt be a problem because the data tells us when its done
-  $("div.sttip").remove();
-  // get next from followers field!
-  const currentStep = stepsData[stepIndex];
+const findFirstStepByOrdinal = (stepsArray) => {
+  const firstStep = stepsArray.reduce((prev, curr) =>
+    prev.action.stepOrdinal < curr.action.stepOrdinal ? prev : curr
+  );
+  if (
+    stepsArray.some(
+      (step) =>
+        step.action.stepOrdinal === firstStep.action.stepOrdinal &&
+        step.uid !== firstStep.uid
+    )
+  ) {
+    //  two or more steps with a minimum ordinal, sort them by id
+    // Ask about this TODO
+  }
+  return firstStep;
+};
+
+const createTipDiv = (currentStep, stepIndex, stepsCount, tiplates) => {
+  console.log(currentStep);
+  // Todo not sure this is right
+  if (currentStep.action.type === "closeScenario") {
+    $("div.sttip").remove();
+    return;
+  }
+  if (currentStep.action.onlyOneTip) {
+    $("div.sttip").remove();
+  }
+  // Todo
   let tip = $(tiplates.tip);
   tip
     .find("div[data-iridize-id=content]")
@@ -32,40 +56,96 @@ const createTipDiv = (stepsData, stepIndex, stepsCount, tiplates) => {
   tip.find("span[data-iridize-role=stepsCount]").html(stepsCount);
 
   const tipDiv =
-    `<div class="sttip"><div class="tooltip ${currentStep.action.classes} in ${currentStep.action.placement} panel-container"><div class="popover-inner guide-content">` +
+    `<div id=${currentStep.uid} class="sttip"><div class="tooltip ${currentStep.action.classes} in ${currentStep.action.placement} panel-container"><div class="popover-inner guide-content">` +
     tip.html() +
     "</div></div></div>";
 
   tip.addClass(currentStep.action.classes);
 
   // Adding the new tooltip to the selector's parent
-  $(currentStep.action["selector"]).parent().append(tipDiv);
+  $(currentStep.action["selector"]).last().parent().append(tipDiv);
+  let tipDomOjbect = $("#" + currentStep.uid);
 
-  // $("button[data-iridize-role=laterBt]").on("click", () =>
-  //   $("div.tooltip").addClass("showLaterBt")
-  // );
-
-  if (currentStep.next) {
-    $(currentStep.next.selector).on(currentStep.next.event, () =>
-      createTipDiv(stepsData, stepIndex + 1, stepsCount, tiplates)
-    );
-  }
   if (currentStep.action.roleTexts) {
     Object.keys(currentStep.action.roleTexts).forEach((role) => {
-      $(`[data-iridize-role=${role}]`).text(currentStep.action.roleTexts[role]);
+      tipDomOjbect
+        .find(`[data-iridize-role=${role}]`)
+        .text(currentStep.action.roleTexts[role]);
     });
   }
-  // adding listeners
-  $("button[data-iridize-role=closeBt]").on("click", () =>
-    $("div.sttip").remove()
-  );
 
-  $("a[data-iridize-role=nextBt]").on("click", () =>
-    createTipDiv(stepsData, stepIndex + 1, stepsCount, tiplates)
-  );
-  $("button[data-iridize-role=prevBt]").on("click", () =>
-    createTipDiv(stepsData, stepIndex - 1, stepsCount, tiplates)
-  );
+  // adding listeners
+  tipDomOjbect
+    .find("button[data-iridize-role=closeBt]")
+    .on("click", () => $("#" + currentStep.uid).remove());
+
+  tipDomOjbect
+    .find("button[data-iridize-role=laterBt]")
+    .on("click", () => $("#" + currentStep.uid).remove());
+
+  // SHOULD ADDRESS CONFITION AND NEXT IN A GENERIC WAY! TODO
+  // create one event listener for all conditioned followers
+  tipDomOjbect.find("a[data-iridize-role=nextBt]").on("click", () => {
+    currentStep.followers.forEach((follower) => {
+      var objIndex = globalStepsArray.findIndex(
+        (step) => step.id === follower.next
+      );
+      globalStepsArray[objIndex].previous = currentStep.id;
+      if (follower.condition && follower["next"]) {
+        createTipDiv(
+          globalStepsArray[objIndex],
+          stepIndex + 1,
+          stepsCount,
+          tiplates
+        );
+      }
+    });
+  });
+
+  // if the follower doesnt have a condition, create it now
+  currentStep.followers.forEach((follower) => {
+    if (!follower.condition) {
+      var objIndex = globalStepsArray.findIndex(
+        (step) => step.id === follower.next
+      );
+      globalStepsArray[objIndex].previous = currentStep.id;
+      createTipDiv(
+        globalStepsArray[objIndex],
+        stepIndex + 1,
+        stepsCount,
+        tiplates
+      );
+    }
+  });
+
+  // create parent tip when clicking back
+  tipDomOjbect.find("button[data-iridize-role=prevBt]").on("click", () => {
+    if (currentStep.previous) {
+      var objIndex = globalStepsArray.findIndex(
+        (step) => step.id === currentStep.previous
+      );
+      createTipDiv(
+        globalStepsArray[objIndex],
+        stepIndex - 1,
+        stepsCount,
+        tiplates
+      );
+    }
+  });
+
+  // Todo not sure this is right
+  if (currentStep.action.watchSelector) {
+    $(currentStep.action["selector"])
+      .first()
+      .on("click", () => {
+        tipDomOjbect.find("a[data-iridize-role=nextBt]").trigger("click");
+      });
+  }
+  if (currentStep.next) {
+    $(currentStep.next.selector).on(currentStep.next.event, () =>
+      tipDomOjbect.find("a[data-iridize-role=nextBt]").trigger("click")
+    );
+  }
 };
 
 const createGLS = async () => {
@@ -82,7 +162,13 @@ const createGLS = async () => {
         (step) => step.action.type === "tip"
       ).length;
       if (tipsCount) {
-        createTipDiv(res.data.structure.steps, 0, tipsCount, res.data.tiplates);
+        globalStepsArray = res.data.structure.steps;
+        const firstStep = findFirstStepByOrdinal(globalStepsArray);
+        var objIndex = globalStepsArray.findIndex(
+          (step) => step.id === firstStep.id
+        );
+        globalStepsArray[objIndex].previous = null;
+        createTipDiv(firstStep, 0, tipsCount, res.data.tiplates);
       }
     }
   } catch (err) {
